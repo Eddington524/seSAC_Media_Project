89ab49140c9b389e34e09ad3c586d7678860d406
eddington524 <iamsam8687@gmail.com>
2024-02-02 20:14:01 +0900

    feat: Hompage

---
+++ seSAC_Media_Project/Base/BaseTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  BaseTableViewCell.swift
+//  seSAC_Media_Project
+//
+//  Created by Sammy Jung on 2/2/24.
+//
+
+import Foundation
---
+++ seSAC_Media_Project/TV/TvTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TvTableViewCell.swift
+//  seSAC_Media_Project
+//
+//  Created by Sammy Jung on 2/2/24.
+//
+
+import UIKit
+
+class TvTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
